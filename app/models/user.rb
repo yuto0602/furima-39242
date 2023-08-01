8509ee0cef193071b2cb955ecf58dd4a22ef7dd2
yuto0602 <rinrin.abc1818@gmail.com>
2023-08-01 18:44:04 +0900

    新規登録、ログイン、ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 
-  validates :encrypted_password, presence: true, length: { maximum: 6 }, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[0-9])[a-zA-Z0-9]+\z/, message: "は半角英数字の混合で入力してください" }
+  # validates :encrypted_password, presence: true, length: { maximum: 6 }, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[0-9])[a-zA-Z0-9]+\z/, message: "は半角英数字の混合で入力してください" }
 end
