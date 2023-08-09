baa7e9f78bc8bae5391c9672f8a0249abc58fff6
yuto0602 <rinrin.abc1818@gmail.com>
2023-08-09 12:00:50 +0900

    エラーハンドリングの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,16 @@ class User < ApplicationRecord
   has_many :orders
 
   # validates :encrypted_password, presence: true, length: { maximum: 6 }, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[0-9])[a-zA-Z0-9]+\z/, message: "は半角英数字の混合で入力してください" }
+
+    # 苗字のバリデーション
+    validates :user_last_name, presence: true, length: { maximum: 50 }, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+
+    # 名前のバリデーション
+    validates :user_first_name, presence: true, length: { maximum: 50 }, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  
+    # カタカナ苗字のバリデーション
+    validates :user_last_name_add, presence: true, length: { maximum: 50 }, format: { with: /\A[ァ-ンー]+\z/ }
+  
+    # カタカナ名前のバリデーション
+    validates :user_first_name_add, presence: true, length: { maximum: 50 }, format: { with: /\A[ァ-ンー]+\z/ }
 end
