7a3dda93a2cbcf2324aa12561f143147988a8496
yuto0602 <rinrin.abc1818@gmail.com>
2023-08-16 12:59:53 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_cndition_id, :delivery_charge_id, :from_id, :date_of_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 end
