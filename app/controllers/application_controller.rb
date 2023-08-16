1db4574b8909e65ebb46ad535208149d2a831ac1
yuto0602 <rinrin.abc1818@gmail.com>
2023-08-16 09:56:07 +0900

    ユーザー認証についての変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,14 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :user_last_name, :user_first_name, :user_last_name_add,
+                                             :user_first_name_add, :birthday])
+  end
+
 end
