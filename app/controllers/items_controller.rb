6e07e56ed19109a337a002d502cd55d7666dbdd1
yuto0602 <rinrin.abc1818@gmail.com>
2023-08-23 19:18:11 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
   
   def index
     @items = Item.order(created_at: :desc)
@@ -19,22 +20,19 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless user_signed_in? && current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
-      render :new
+      render :edit
     end
   end
 
@@ -46,4 +44,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_condition_id, :delivery_charge_id, :from_id, :date_of_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
