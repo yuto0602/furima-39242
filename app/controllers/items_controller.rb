793ff627ffd4e85f7fb3f5798b47afaa09ee4ac5
yuto0602 <rinrin.abc1818@gmail.com>
2023-08-23 17:48:44 +0900

    編集機能のコメントアウト解除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,24 +22,24 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  # def edit
-  #   @item = Item.find(params[:id])
-  #   unless user_signed_in? && current_user.id == @item.user_id
-  #     redirect_to action: :index
-  #   end
-  # end
+  def edit
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
 
-  # def update
-  #   @item = Item.find(params[:id])
-  #   if @item.update(item_params)
-  #     redirect_to item_path
-  #   else
-  #     render :new
-  #   end
-  # end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :new
+    end
+  end
 
-  # def destroy
-  # end
+  def destroy
+  end
 
   private
   def item_params
