a3d2abaab5c8b1cb8cfefa7efa34c98a9b3a28f9
yuto0602 <rinrin.abc1818@gmail.com>
2023-08-25 10:23:20 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2023_08_16_073853) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
